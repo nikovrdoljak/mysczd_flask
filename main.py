f8492669f6c6ffd18cc0f6d098dc32caece764dc
Niko Vrdoljak <niko@mailtastic.com>
2020-10-12 21:34:31 +0200

    open weather api

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import babel
+from babel.dates import format_datetime, get_timezone
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello my smart city Zadar!!!</h1>"
+    # https://openweathermap.org/api
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    parameters = {'id': '3186952', 'appid': 'VAS_APP_ID', 'units': 'metric', 'lang': 'hr'}
+    response = requests.get(url, parameters)
+    weather = response.json()
+    return render_template('index.html', weather = weather)
+
+@app.template_filter('datetime')
+def format_datetime(value, format='medium'):
+    zadar = get_timezone('Europe/Berlin')
+    if format == 'full':
+        format="EEEE, d. MMMM y 'at' HH:mm"
+    elif format == 'time':
+        format="HH:mm"
+    return babel.dates.format_datetime(value, format, tzinfo=zadar)
